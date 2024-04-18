24348af2007c2af74a55a122da31accaa09258c6
Ian <100783092+IanRRocha@users.noreply.github.com>
2024-04-17 21:56:27 -0300

    Update Troco.java

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -14,33 +14,30 @@ class Troco {
         papeisMoeda = new PapelMoeda[6];
         int count = valor / 100;
         papeisMoeda[5] = new PapelMoeda(100, count);
+        valor %= 100;
 
         count = 0;
-
         count = valor / 50; 
-
-        
         papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-
-        count = valor / 20; 
-
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        valor %= 20; 
-
-        count = 0;
+        valor %= 50;
         
+        count = 0;
+        count = valor / 20; 
+        papeisMoeda[3] = new PapelMoeda(20, count);
+        valor %= 20;
+        
+
+        count = 0;
         count = valor / 10; 
         papeisMoeda[2] = new PapelMoeda(10, count);
-
-        count = 0;
-       
-        count = valor / 5;
-
-        papeisMoeda[1] = new PapelMoeda(5, count);
-
-        count = 0;
+        valor %= 10;
         
+        count = 0;
+        count = valor / 5;
+        papeisMoeda[1] = new PapelMoeda(5, count);
+        valor %= 5;
+
+        count = 0;
         count = valor / 2;
         papeisMoeda[0] = new PapelMoeda(2, count);
         valor %= 2;
